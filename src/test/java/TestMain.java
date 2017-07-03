ac854d998322630476cf302d77cbf9e1164c4619
ruiduan.li <ruiduan.li@ele.me>
2017-07-03 15:57:57 +0800

    thread Test

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,8 +1,21 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by ruidli on 2017/6/12.
  */
 public class TestMain {
-    public static void main(String args[]){
-
+    public static void main(String args[]) throws Exception {
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("uncaught exception");
+            }
+        });
+        ExecutorService abc = Executors.newFixedThreadPool(6);
+        abc.execute(()->{
+            System.out.println("thread exec");
+                throw new Exception();
+        });
     }
 }
--- src/test/java/javaTest.java
+++ src/test/java/javaTest.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by ruidli on 2017/6/14.
@@ -12,6 +13,7 @@ public class javaTest {
         list.add(30);
         list.add(40);
         list.add(50);
+       List list1 =  list.stream().filter(f->f>20).map(f->f.longValue()).collect(Collectors.toList());
         System.out.print(list.contains(2));
 
     }
